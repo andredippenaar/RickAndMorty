f73c8e3b63faf96908f5dff683e7f2f40160e083
Andre Dippenaar <andre.pierre.dippenaar@gmail.com>
2023-01-10 13:30:58 +0200

    Added docstrings and API related files

---
+++ RickAndMorty/APIClient/RMRequest.swift
@@ -0,0 +1,13 @@
+//
+//  RMRequest.swift
+//  RickAndMorty
+//
+//  Created by Andre Dippenaar on 2023/01/10.
+//
+
+import Foundation
+
+/// Object that represents a single API call
+final class RMRequest {
+    
+}
